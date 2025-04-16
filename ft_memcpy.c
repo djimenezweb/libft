f2420479f73c66adda7706c87e8ec361c0591ec6
djimenezweb <djimenezweb@gmail.com>
2025-04-16 10:41:44 +0200

    checked for null params

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 10:18:23 by danielji          #+#    #+#             */
-/*   Updated: 2025/04/15 14:19:27 by danielji         ###   ########.fr       */
+/*   Updated: 2025/04/16 10:41:20 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -17,6 +17,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*d;
 	unsigned char	*s;
 
+	if (!dest && !src)
+	{
+		return ((void *)0);
+	}
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
 	while (n > 0)
