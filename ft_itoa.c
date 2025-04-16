66a11f56958f48238f6e7c9818c9ee6e341aaee2
djimenezweb <djimenezweb@gmail.com>
2025-04-16 12:36:34 +0200

    Converted n to long long

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 15:28:40 by danielji          #+#    #+#             */
-/*   Updated: 2025/04/16 12:23:46 by danielji         ###   ########.fr       */
+/*   Updated: 2025/04/16 12:35:43 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -14,34 +14,38 @@
 
 static int	numlen(int n)
 {
-	int	len;
+	int				len;
+	long long	num;
 
 	len = 0;
-	if (n == 0)
+	num = n;
+	if (num == 0)
 	{
 		len++;
 	}
-	while (n)
+	while (num)
 	{
 		len++;
-		n /= 10;
+		num /= 10;
 	}
 	return (len);
 }
 
 char	*ft_itoa(int n)
 {
-	int		len;
-	int		is_neg;
-	char	*str;
+	int				len;
+	int				is_neg;
+	char			*str;
+	long long	num;
 
+	num = n;
 	is_neg = 0;
-	if (n < 0)
+	if (num < 0)
 	{
 		is_neg = 1;
-		n *= -1;
+		num *= -1;
 	}
-	len = numlen(n) + is_neg;
+	len = numlen(num) + is_neg;
 	str = malloc(len + 1);
 	if (!str)
 		return ((void *)0);
@@ -49,18 +53,20 @@ char	*ft_itoa(int n)
 	len--;
 	while (len >= 0)
 	{
-		str[len] = (n % 10) + '0';
+		str[len] = (num % 10) + '0';
 		len--;
-		n = n / 10;
+		num = num / 10;
 	}
 	if (is_neg)
 		str[0] = '-';
 	return (str);
 }
 
-int	main(void)
+/* int	main(void)
 {
-	char *str = ft_itoa(0);
+	//          -2147483648LL
+	// expects: -2147483648
+	char *str = ft_itoa(-2147483648LL);
 	ft_putstr_fd(str, 1);
 	free(str);
-}
+} */
