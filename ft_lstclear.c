79d01401da9e7f6ac26ceb349b9f4dc1f875f54e
Daniel J. <117752348+djimenezweb@users.noreply.github.com>
2025-04-17 19:10:45 +0200

    Add files via upload

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,25 +6,33 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 11:29:41 by danielji          #+#    #+#             */
-/*   Updated: 2025/04/17 15:15:14 by danielji         ###   ########.fr       */
+/*   Updated: 2025/04/17 19:06:18 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
-/* 
-
-Elimina y libera el nodo ’lst’ dado y todos los consecutivos de ese nodo,
+/* Elimina y libera el nodo ’lst’ dado y todos los consecutivos de ese nodo,
 utilizando la función ’del’ y free(3).
 Al final, el puntero a la lista debe ser NULL.
 
 lst: la dirección de un puntero a un nodo.
 
-del: un puntero a función utilizado para eliminar el contenido de un nodo.
-
-*/
+del: un puntero a función utilizado para eliminar el contenido de un nodo.*/
 
 #include "libft.h"
 
 void ft_lstclear(t_list **lst, void (*del)(void *))
 {
+	t_list	*node;
+	t_list	*next;
 
+	node = *lst;
+	while (node)
+	{
+		next = node;
+		node = node->next;
+		del(next);
+		free(next);
+	}
+	// Al final, el puntero a la lista debe ser NULL.
+	// **lst = ((void )*0);
 }
--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,23 +6,26 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 11:30:59 by danielji          #+#    #+#             */
-/*   Updated: 2025/04/17 11:35:17 by danielji         ###   ########.fr       */
+/*   Updated: 2025/04/17 19:08:57 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
-/* 
-
-Itera la lista ’lst’ y aplica la función ’f’ en el contenido de cada nodo.
+/* Itera la lista ’lst’ y aplica la función ’f’ en el contenido de cada nodo.
 
 lst: un puntero al primer nodo.
 
-f: un puntero a la función que utilizará cada nodo.
-
-*/
+f: un puntero a la función que utilizará cada nodo. */
 
 #include "libft.h"
 
 void ft_lstiter(t_list *lst, void (*f)(void *))
 {
+	t_list	*ptr;
 
+	ptr = lst;
+	while(ptr)
+	{
+		f(ptr);
+		ptr = ptr->next;
+	}
 }
